5d1eedbf512986e28db27ebd112032994b837f95
Mohamad Hilmi Rinaldi <aldi280402@gmail.com>
2023-12-17 00:30:59 +0700

    feat: add auth jwt on get user profile

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { saveUserProfile, updateUserProfile, updateDailyNutrition, getDailyNutrition, getUserProfileByUsername} = require('../controllers/userProfileController');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post("/user-profile", saveUserProfile);
 router.put("/user-profile", updateUserProfile);
-router.get("/user-profile/:username", getUserProfileByUsername);
+router.get("/user-profile/:username", authMiddleware, getUserProfileByUsername);
 router.put("/nutrition-user", updateDailyNutrition);
-router.get("/nutrition-user/:username", getDailyNutrition);
+router.get("/nutrition-user/:username", authMiddleware, getDailyNutrition);
 
 module.exports = router;
