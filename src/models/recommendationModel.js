c110bb7d5d9ed0c3d95d6e13436e678266da34d5
Steven Wen <73114262+loopfree@users.noreply.github.com>
2023-12-18 22:09:46 +0700

    feat: update recommendation system logic

--- src/models/recommendationModel.js
+++ src/models/recommendationModel.js
@@ -1,10 +1,42 @@
 const pool = require("../config/database")
 
-async function getFoodByCalories(calories) {
-    const sql = "SELECT * FROM Foods WHERE calories <= ?"
-    const holder =  await pool.execute(sql, [calories])
+function roundToDown100(number) {
+    return Math.round(number/100) * 100
+}
 
-    return holder[0][holder[0].length - 1]
+async function getFoodByCalories(calories) {
+    let lowerBound
+    let upperBound
+    if (calories <= 600) {
+        lowerBound = Math.max(0, roundToDown100(calories - 200))
+        upperBound = Math.min(roundToDown100(calories), roundToDown100(calories + 100))
+    } else { // calories > 500
+        lowerBound = Math.max(0, roundToDown100(calories - 400))
+        upperBound = Math.min(roundToDown100(calories), roundToDown100(calories + 100))
+    }
+    console.log(lowerBound)
+    console.log(upperBound)
+
+    const sql = "SELECT * FROM Foods WHERE calories BETWEEN ? AND ?"
+    const holder =  await pool.execute(sql, [lowerBound, upperBound])
+
+    let randomizer1 = Math.floor(Math.random()*holder[0].length)
+
+    let randomizer2 = Math.floor(Math.random()*holder[0].length)
+    while (randomizer2 == randomizer1) {
+        randomizer2 = Math.floor(Math.random()*holder[0].length)
+    }
+
+    let randomizer3 = Math.floor(Math.random()*holder[0].length)
+    while (randomizer3 == randomizer1 || randomizer3 == randomizer2) {
+        randomizer3 = Math.floor(Math.random()*holder[0].length)
+    }
+
+    return { 
+        recomendation1: holder[0][randomizer1], 
+        recomendation2: holder[0][randomizer2], 
+        recomendation3: holder[0][randomizer3] 
+    }
 }
 
 module.exports = {
