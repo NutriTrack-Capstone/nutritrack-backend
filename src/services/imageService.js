1e712a8ccb8f0677d4d5c885cf165270c2fad497
Steven Wen <73114262+loopfree@users.noreply.github.com>
2023-12-20 23:45:25 +0700

    feat: update converter to multiplatform

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,41 +1,38 @@
-const Image = require("image-js");
-const sharp = require("sharp")
+const Jimp = require("jimp");
+const fs = require("fs");
 
 async function imageBufferTo3DArray(imageBuffer) {
     try {
-        // Convert image buffer to RGB buffer
-        const rgbBuffer = await sharp(imageBuffer).ensureAlpha().raw().toBuffer({ resolveWithObject: true });
-  
+        // Read the image with Jimp
+        const image = await Jimp.read(imageBuffer);
+
         // Get image data
-        const { data, info } = rgbBuffer;
-  
+        const { width, height, data } = image.bitmap;
+
         // Convert image data to a 3D array
         const imageArray = [];
-        let index = 0;
-  
-        for (let y = 0; y < info.height; y++) {
+
+        for (let y = 0; y < height; y++) {
             const row = [];
-            for (let x = 0; x < info.width; x++) {
+            for (let x = 0; x < width; x++) {
                 const pixel = [];
                 for (let c = 0; c < 3; c++) {
-                    pixel.push(data[index]);
-                    index++;
+                    pixel.push(data[y * width * 4 + x * 4 + c]);
                 }
                 row.push(pixel);
             }
             imageArray.push(row);
         }
-  
+
         return imageArray;
     } catch (error) {
         console.error('Error converting image to 3D array:', error.message);
     }
 }
 
-const fs = require("fs")
-const imagePath = "../../img/lemon.png"
-const imageBuffer = fs.readFileSync(imagePath)
+const imagePath = "../../img/lemon.png";
+const imageBuffer = fs.readFileSync(imagePath);
 
 imageBufferTo3DArray(imageBuffer).then((imageArray) => {
-    console.log(imageArray)
-})
+    console.log(imageArray);
+});
